356ded548ac3b51c40c9d35aa9edc2fc69d17857
GomZ-10 <gomezdiego09@gmail.com>
2022-03-07 18:23:03 -0500

    key for snake 2

--- snake/game/scripting/control_actors_action.py
+++ snake/game/scripting/control_actors_action.py
@@ -48,5 +48,21 @@ class ControlActorsAction(Action):
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._direction)
 
-        #DIEGO- duplicate the above if statements and update the key 'l','j','i','k'
+      
         # call snake #2 after if statements
+
+         # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+        
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+        
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+        
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction = Point(0, constants.CELL_SIZE)
